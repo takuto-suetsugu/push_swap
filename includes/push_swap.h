def88302200897ebc3e9b2cac856ad0863948fe0
Takuto Suetsugu <tsuetsug@c1r5s1.42tokyo.jp>
2021-08-09 18:01:23 +0900

    Create: AddStackto CreateList

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tsuetsug <tsuetsug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 14:38:15 by tsuetsug          #+#    #+#             */
-/*   Updated: 2021/08/09 16:02:14 by tsuetsug         ###   ########.fr       */
+/*   Updated: 2021/08/09 17:30:36 by tsuetsug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,10 @@
 int main(int argc, char **argv);
 typedef struct  linked_stack
 {
-    long     number;
-    struct  linked_stack* next_stack;
+	long					number;
+	struct linked_stack*	next;
+	struct linked_stack*	prev;
 }               t_stacks;
-struct linked_stack *first_stack = NULL;
+
 
 #endif
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tsuetsug <tsuetsug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/30 17:35:27 by tsuetsug          #+#    #+#             */
-/*   Updated: 2021/08/09 16:02:37 by tsuetsug         ###   ########.fr       */
+/*   Updated: 2021/08/09 18:00:27 by tsuetsug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,27 +71,44 @@ int IsAscending(int argc, char **argv)
 	return (1);
 }
 */
-
-int	AddList(int argc, char **argv)
+void	AddStack(int value)
 {
-	struct linked_stack	*new_stack;
+	struct t_stack	*tail;
+	struct t_stack	*new_stack;
+
+	tail = SearchTail();
+	new_stack = malloc (sizeof(struct linked_stack));
+	if (new_stack == NULL)
+		return (1);
+	new_stack->number = value;
+	new_stack->next = NULL;
+	new_stack->prev = tail;
+	tail->next = new_stack;
+}
+
+
+int	CreateList(int argc, char **argv)
+{
 	int					i;
 	long				stack_n;
 
+	t_stack.next = &t_stack;
+	t_stack.prev = &t_stack;
 	i = 1;
 	while (i < argc)
 	{
 		stack_n = ft_atoi(argv[i]);
 		if (stack_n > 2147483647 || stack_n < -2147483648)
 			return (1);
-		new_stack = malloc (sizeof(struct linked_stack));
-		(*new_stack).number = stack_n;
+		AddStack(stack_n);
 	}
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
+	t_stack *first_stack = NULL;
+	
 	if (HasNonInteger(argc, argv) || HasSameNumber(argc, argv))
 	{
 		write(1, "Error\n", 6);
@@ -102,7 +119,7 @@ int	main(int argc, char **argv)
 		write(1, "\n", 1);
 		return (0);
 	}
-	if (AddList(argc, argv))
+	if (CreateList(argc, argv, linked_stack))
 	{
 		write(1, "Error\n", 6);
 		return (0);
