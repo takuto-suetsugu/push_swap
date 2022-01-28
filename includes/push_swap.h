16bccee4009ee5511d72e4d4ded027ca117cd3e3
末次拓斗 <s.tact330@gmail.com>
2022-01-28 10:56:44 +0900

    些細な変更

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tsuetsug < tsuetsug@student.42tokyo.jp>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 14:38:15 by tsuetsug          #+#    #+#             */
-/*   Updated: 2022/01/28 10:55:12 by tsuetsug         ###   ########.fr       */
+/*   Updated: 2022/01/28 10:56:41 by tsuetsug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 typedef struct  s_node
 {
 	long			num;
 	struct s_node*	next;
 	struct s_node*	prev;
 }               t_node;
-
 typedef	struct s_command
 {
 	char				command[4];
@@ -34,24 +32,20 @@ typedef	struct s_command
 int		main(int argc, char **argv);
 int		HasNonInteger(int argc, char **argv);
 int		HasSameNumber(int argc, char **argv);
-
 void	SortProcess(t_node *guard_A, t_node *guard_B,
-		t_command *guard_command);
-
+						t_command *guard_command);
 void	CreateNode(long node_n, t_node *guard_node);
 void	AddNode(t_node *add_node, t_node *guard_node);
 void	DeleteNode(t_node *guard_node);
 void	SwapNode(t_node *guard_node);
 void	RotateNode(t_node *guard_node);
 void	ReverseRotateNode(t_node *guard_node);
-
 void	PrintCommand(t_command *guard_command);
 void    AddCommand(t_command *guard_command, char *type);
 void    SA_SB(t_node *guard_node, t_command *guard_command);
 void	RA_RB(t_node *guard_node, t_command *guard_command);
 void	RRA_RRB(t_node *guard_node, t_command *guard_command);
 void	PA_PB(t_node *guard_src, t_node *guard_dst, t_command *guard_command);
-
 int		IsAscending(t_node *guard_node);
 int		IsDescending(t_node *guard_node);
 int		CountNode(t_node *guard_node);
@@ -63,18 +57,16 @@ int		HasBigNode(t_node *guard_node, t_node *pivot);
 void	InsertNode(t_node *guard_src, t_node *guard_dst, t_command *guard_command);
 void	OptimizeRotateMinTop(t_node *guard_src, t_command *guard_command);
 t_node	*SearchMedian(t_node *guard_node);
-
-
 t_node	*PASorted(t_node *sorted_head, t_node *guard_A, t_node *guard_B,
-		t_command *guard_command);
+					t_command *guard_command);
 void	OptimizeRotateTailBottom(t_node *tail, t_node *guard,
-		t_command *guard_command);
+									t_command *guard_command);
 void	PABigThanPivot(t_node *pivot_node, t_node *guard,
-		t_node *guard_dst, t_command *guard_command);
+						t_node *guard_dst, t_command *guard_command);
 void	PBSmallThanPivot(t_node *pivot_node, t_node *guard,
-		t_node *guard_dst, t_command *guard_command);
+							t_node *guard_dst, t_command *guard_command);
 t_node	*RBIfMin(t_node *guard, t_node *guard_dst,
-		t_node *sorted_head, t_command *guard_command);
+					t_node *sorted_head, t_command *guard_command);
 t_node	*RAIfMin(t_node *guard, t_node *sorted_head, t_command *guard_command);
 
 void	PrintNumber(t_node *guard);
